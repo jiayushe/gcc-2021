99b810567696599ad463b7e8e0972e844a602938
jiayushe <shejiayu1998@gmail.com>
2021-10-02 20:44:16 +0800

    Add q1

--- Question1/Question1.cpp
+++ Question1/Question1.cpp
@@ -1,10 +1,14 @@
-
 using namespace std;
 #include <string>
 #include <iostream>
 
 int solution(int n){
     // code here
+    int ans = n / 3 * 2;
+    if(n % 3) {
+		ans++;
+	}
+    return ans;
 }
 
 /*  Do not edit below code */
