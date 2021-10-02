35634747bf8a630f731c11b50333746dd06f44b6
jiayushe <shejiayu1998@gmail.com>
2021-10-02 20:44:49 +0800

    Add q3

--- Question3/Question3.cpp
+++ Question3/Question3.cpp
@@ -1,8 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int portfolio_profit_maximisation(int maxSum, vector<int> a, vector<int> b) {
+int portfolio_profit_maximisation(int x, vector<int> a, vector<int> b) {
     //write your code here
+    int n = a.size(), m = b.size();
+    if (n > m) {
+		swap(a, b);
+		swap(n, m);
+	}
+	vector<int> ap, bp;
+	ap.push_back(0);
+	for(int i = 0; i < n; i++) {
+		if(a[i] > x - ap[i]) {
+			break;
+		}
+		ap.push_back(ap[i] + a[i]);
+	}
+	bp.push_back(0);
+	for(int i = 0; i < m; i++) {
+		if(b[i] > x - bp[i]) {
+			break;
+		}
+		bp.push_back(bp[i] + b[i]);
+	}
+	n = ap.size();
+	int ans = 0;
+	for(int i = 0; i < n; i++) {
+		int j = upper_bound(bp.begin(), bp.end(), x - ap[i]) - bp.begin();
+		ans = max(ans, i + j - 1);
+	}
+	return ans;
 }
 
 int main()
@@ -22,7 +49,7 @@ int main()
     for (int i = 0; i < m; ++i) {
         cin >> b[i];
     }
-    int result = portfolio_profit_maximisation(maxSum, a, b);
+    int result = portfolio_profit_maximisation(x, a, b);
     cout << result;
     return 0;
 }
