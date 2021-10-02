b425f86a620166a69fd4dc7c754d513910cf7930
jiayushe <shejiayu1998@gmail.com>
2021-10-02 23:25:11 +0800

    Add q5

--- Question5/Question5.cpp
+++ Question5/Question5.cpp
@@ -4,8 +4,23 @@ using namespace std;
 
 string solution(string n){
     // code here
-    //return "A";
-    //return "B"
+    int m = n.length();
+    int one = 0, two = 0;
+    for(int i = 0; i < m; i++) {
+		int j = i;
+		while(j < m && n[j] == '0') {
+			j++;
+		}
+		int curr = j - i;
+		if(curr > one) {
+			two = one;
+			one = curr;
+		} else if(curr > two) {
+			two = curr;
+		}
+		i = j;
+	}
+	return (one % 2 && one / 2 + 1 > two) ? "A" : "B";
 }
 
 /*  Do not edit below code */
