c831a85eb808b17063309a5c38ad79776c3e579a
jiayushe <shejiayu1998@gmail.com>
2021-10-04 02:00:43 +0800

    Add q8

--- Question8/Question8.cpp
+++ Question8/Question8.cpp
@@ -1,20 +1,45 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int calculateMaximizedReturns(int n, int k, int d, int m, vector<int> returns) {
-    // Participant's code will go here
-    return -1;
+
+const int inf = 1e9;
+int n, k, d, m;
+vector<int> returns;
+vector<vector<int>> rsum;
+vector<vector<int>> memo;
+
+int solve(int curr, int trans) {
+	int &ans = memo[curr][trans];
+	if(ans != -inf) return ans;
+	if(trans == k - 1) {
+		ans = rsum[curr + 1][n - 1];
+	} else {
+		for(int i = curr + 1; i <= min(n - 1, n - k + trans + 1); i++) {
+			ans = max(ans, rsum[curr + 1][i - 1] + solve(i, trans + 1));
+		}
+	}
+	return ans;
 }
 
-vector<string> split(const string& str, char delim) {
-    vector<string> strings;
-    size_t start;
-    size_t end = 0;
-    while ((start = str.find_first_not_of(delim, end)) != string::npos) {
-        end = str.find(delim, start);
-        strings.push_back(str.substr(start, end - start));
-    }
-    return strings;
+int calculateMaximizedReturns() {
+    // Participant's code will go here
+    rsum.assign(n + 1, vector<int>(n + 1, 0));
+    rsum[0][0] = returns[0];
+    for(int j = 1; j < n; j++) {
+		rsum[0][j] = rsum[0][j - 1] + returns[j];
+	}
+    for(int i = 1; i < n; i++) {
+		rsum[i][i] = returns[i] * m;
+		for(int j = i + 1; j < n; j++) {
+			rsum[i][j] = rsum[i][j - 1] + returns[j] * (j - i < d ? m : 1);
+		}
+	}
+    memo.assign(n, vector<int>(k, -inf));
+    int ans = solve(0, 0);
+    for(int i = 1; i <= n - k; i++) {
+		ans = max(ans, rsum[0][i - 1] + solve(i, 0));
+	}
+    return ans;
 }
 
 vector<int> splitStringToInt(const string& str, char delim) {
@@ -28,36 +53,22 @@ vector<int> splitStringToInt(const string& str, char delim) {
     return strings;
 }
 
-void printVector(vector<int> vec) {
-    for (vector<int>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
-        cout << *i << ' ';
-    }
-    cout << endl;
-}
-
-void printVector(vector<string> vec) {
-    for (vector<string>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
-        cout << *i << ' ';
-    }
-    cout << endl;
-}
-
 int main() {
     string firstLine;
     getline(cin, firstLine);
 
     vector<int> firstLineVec = splitStringToInt(firstLine, ' ');
-    int n = firstLineVec[0];
-    int k = firstLineVec[1];
-    int d = firstLineVec[2];
-    int m = firstLineVec[3];
+    n = firstLineVec[0];
+    k = firstLineVec[1];
+    d = firstLineVec[2];
+    m = firstLineVec[3];
 
-    string returns;
-    getline(cin, returns);
+    string returnsStr;
+    getline(cin, returnsStr);
 
-    vector<int> returnsVec = splitStringToInt(returns, ' ');
+    returns = splitStringToInt(returnsStr, ' ');
 
-    int result = calculateMaximizedReturns(n, k, d, m, returnsVec);
+    int result = calculateMaximizedReturns();
 
     // Do not remove below line
     cout << result << "\n";
