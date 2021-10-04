c38ce9dc929d8ce65b45728047964c97a651ee03
jiayushe <shejiayu1998@gmail.com>
2021-10-04 10:20:38 +0800

    Clean up q6

--- Question6/Question6.cpp
+++ Question6/Question6.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-vector<string> split_string(string);
-
 int find(vector<int>& d, int a) {
     if(d[a] == -1) return a;
     return d[a] = find(d, d[a]);
@@ -17,120 +15,92 @@ void join(vector<int>& d, int a, int b) {
 }
 
 void theHackathon(int n, int m, int a, int b, int f, int s, int t) {
-    // Participant code here
     string x, y;
     int g;
     unordered_map<int, string> idx_nme;
     unordered_map<string, int> nme_idx;
-	vector<int> d(n, -1), cnt(n, 1), fcnt(n, 0), scnt(n, 0), tcnt(n, 0);
+    vector<int> d(n, -1), cnt(n, 1), fcnt(n, 0), scnt(n, 0), tcnt(n, 0);
     for(int i = 0; i < n; i++) {
-		cin>>x>>g;
-		idx_nme[i] = x;
-		nme_idx[x] = i;
-		switch(g) {
-			case 1:
-				fcnt[i]++;
-				break;
-			case 2:
-				scnt[i]++;
-				break;
-			default:
-				tcnt[i]++;
-		}
-	}
-	for(int i = 0; i < m; i++) {
-		cin>>x>>y;
-		int xg = find(d, nme_idx[x]), yg = find(d, nme_idx[y]);
-		if(xg != yg && cnt[xg] + cnt[yg] <= b && fcnt[xg] + fcnt[yg] <= f
-			&& scnt[xg] + scnt[yg] <= s && tcnt[xg] + tcnt[yg] <= t) {
-			join(d, xg, yg);
-			cnt[yg] += cnt[xg];
-			fcnt[yg] += fcnt[xg];
-			scnt[yg] += scnt[xg];
-			tcnt[yg] += tcnt[xg];
-		}
-	}
-	int mx = a - 1;
-	vector<int> idxs;
-	for(int i = 0; i < n; i++) {
-		if(cnt[i] > mx) {
-			mx = cnt[i];
-			idxs = {i};
-		} else if(cnt[i] == mx && idxs.size()) {
-			idxs.push_back(i);
-		}
-	}
-	if(idxs.size() == 0) {
-		cout<<"no groups";
-		return;
-	}
-	int idx = idxs[0];
-	for(auto i : idxs) {
-		join(d, i, idx);
-	}
-	vector<string> res;
-	for(int i = 0; i < n; i++) {
-		if(find(d, i) == idx) {
-			res.push_back(idx_nme[i]);
-		}
-	}
-	sort(res.begin(), res.end());
-	for(auto i : res) {
-		cout<<i<<"\n";
-	}
+        cin>>x>>g;
+        idx_nme[i] = x;
+        nme_idx[x] = i;
+        switch(g) {
+            case 1:
+                fcnt[i]++;
+                break;
+            case 2:
+                scnt[i]++;
+                break;
+            default:
+                tcnt[i]++;
+        }
+    }
+    for(int i = 0; i < m; i++) {
+        cin>>x>>y;
+        int xg = find(d, nme_idx[x]), yg = find(d, nme_idx[y]);
+        if(xg != yg && cnt[xg] + cnt[yg] <= b && fcnt[xg] + fcnt[yg] <= f
+            && scnt[xg] + scnt[yg] <= s && tcnt[xg] + tcnt[yg] <= t) {
+            join(d, xg, yg);
+            cnt[yg] += cnt[xg];
+            fcnt[yg] += fcnt[xg];
+            scnt[yg] += scnt[xg];
+            tcnt[yg] += tcnt[xg];
+        }
+    }
+    int mx = a - 1;
+    vector<int> idxs;
+    for(int i = 0; i < n; i++) {
+        if(cnt[i] > mx) {
+            mx = cnt[i];
+            idxs = {i};
+        } else if(cnt[i] == mx && idxs.size()) {
+            idxs.push_back(i);
+        }
+    }
+    if(idxs.size() == 0) {
+        cout<<"no groups";
+        return;
+    }
+    int idx = idxs[0];
+    for(auto i : idxs) {
+        join(d, i, idx);
+    }
+    vector<string> res;
+    for(int i = 0; i < n; i++) {
+        if(find(d, i) == idx) {
+            res.push_back(idx_nme[i]);
+        }
+    }
+    sort(res.begin(), res.end());
+    for(auto i : res) {
+        cout<<i<<"\n";
+    }
 }
 
-int main()
-{
+vector<int> splitStringToInt(const string& str, char delim) {
+    vector<int> strings;
+    size_t start;
+    size_t end = 0;
+    while ((start = str.find_first_not_of(delim, end)) != string::npos) {
+        end = str.find(delim, start);
+        strings.push_back(stoi(str.substr(start, end - start)));
+    }
+    return strings;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     string inputdata_temp;
     getline(cin, inputdata_temp);
-
-    vector<string> inputdata = split_string(inputdata_temp);
-
-    int n = stoi(inputdata[0]);
-
-    int m = stoi(inputdata[1]);
-
-    int a = stoi(inputdata[2]);
-
-    int b = stoi(inputdata[3]);
-
-    int f = stoi(inputdata[4]);
-
-    int s = stoi(inputdata[5]);
-
-    int t = stoi(inputdata[6]);
-
+    vector<int> inputdata = splitStringToInt(inputdata_temp, ' ');
+    int n = inputdata[0];
+    int m = inputdata[1];
+    int a = inputdata[2];
+    int b = inputdata[3];
+    int f = inputdata[4];
+    int s = inputdata[5];
+    int t = inputdata[6];
     theHackathon(n, m, a, b, f, s, t);
-
     return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
 }
