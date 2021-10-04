158d687c79a38f1c18a746cd5aceca12a683abc2
jiayushe <shejiayu1998@gmail.com>
2021-10-04 22:36:06 +0800

    Improve q7 again

--- Question7/Question7.cpp
+++ Question7/Question7.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <cstring>
 
 using namespace std;
 typedef long long ll;
 
+#define MAXN 100000
+#define MAXV 1000001
+
 const ll mod = 1e9 + 7;
 int prime[] = {0,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997};
-int m = 169;
+int m = 169, n;
+int values[MAXN], d[MAXN];
+int prime_grp[MAXV];
 
 ll twopowmod(ll p) {
     ll res = 1, a = 2;
@@ -18,13 +23,13 @@ ll twopowmod(ll p) {
     return res;
 }
 
-inline int find(vector<int>& d, int a) {
+inline int find(int a) {
     if(d[a] == -1) return a;
-    return d[a] = find(d, d[a]);
+    return d[a] = find(d[a]);
 }
 
-inline void join(vector<int>& d, int a, int b) {
-    a = find(d, a), b = find(d, b);
+inline void join(int a, int b) {
+    a = find(a), b = find(b);
     if(a == b) return;
     d[a] = b;
 }
@@ -32,12 +37,11 @@ inline void join(vector<int>& d, int a, int b) {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    int n;
     cin>>n;
-    vector<int> values(n);
     for(int i = 0; i < n; i++) cin>>values[i];
-    int mx = *max_element(values.begin(), values.end());
-    vector<int> prime_grp(mx + 1, -1), d(n, -1), pf;
+    memset(prime_grp, -1, sizeof prime_grp);
+    memset(d, -1, sizeof d);
+    vector<int> pf;
     int cnt = n;
     for(int i = 0; i < n; i++) {
         pf.clear();
@@ -52,8 +56,8 @@ int main() {
         for(auto p : pf) {
             if(prime_grp[p] == -1) {
                 prime_grp[p] = i;
-            } else if(find(d, i) != find(d, prime_grp[p])) {
-                join(d, i, prime_grp[p]);
+            } else if(find(i) != find(prime_grp[p])) {
+                join(i, prime_grp[p]);
                 cnt--;
             }
         }
