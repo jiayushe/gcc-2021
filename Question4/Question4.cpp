6c1cc0db571547292debebef7978243e3ad69d07
jiayushe <shejiayu1998@gmail.com>
2021-10-04 10:51:20 +0800

    Clean up q4

--- Question4/Question4.cpp
+++ Question4/Question4.cpp
@@ -1,38 +1,35 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
 int totalPairs(int n, vector<int>& values) {
-    // Complete the total pairs function
     vector<int> v;
     int ans = 0;
     for(int i = 0; i < n; i++) {
-		int curr = values[i];
-		while(v.size()) {
-			ans++;
-			int prev = v.back();
-			if(curr >= prev) {
-				v.pop_back();
-			}
-			if(curr <= prev) {
-				break;
-			}
-		}
-		v.push_back(curr);
-	}
+        int curr = values[i];
+        while(v.size()) {
+            ans++;
+            if(curr > v.back()) {
+                v.pop_back();
+            } else {
+                break;
+            }
+        }
+        v.push_back(curr);
+    }
     return ans;
 }
 
-int main(){
-   int n;
-   cin >> n;
-   vector<int> values(n);
-   for (int i = 0; i < n; ++i) {
-        cin >> values[i];
-   }
-	int answer = totalPairs(n, values);
-    // Do not remove below line
-    cout << answer << endl;
-    // Do not print anything after this line
-        
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    vector<int> values(n);
+    for(int i = 0; i < n; ++i) {
+        cin>>values[i];
+    }
+    int answer = totalPairs(n, values);
+    cout<<answer<<endl;
     return 0;
 }
