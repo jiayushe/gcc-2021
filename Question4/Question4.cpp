88e4372f589b36576c5fd118f84611fe5352bfc6
jiayushe <shejiayu1998@gmail.com>
2021-10-04 13:54:27 +0800

    Improve q4

--- Question4/Question4.cpp
+++ Question4/Question4.cpp
@@ -1,35 +1,24 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
 
 using namespace std;
-
-int totalPairs(int n, vector<int>& values) {
-    vector<int> v;
-    int ans = 0;
-    for(int i = 0; i < n; i++) {
-        int curr = values[i];
-        while(v.size()) {
-            ans++;
-            if(curr > v.back()) {
-                v.pop_back();
-            } else {
-                break;
-            }
-        }
-        v.push_back(curr);
-    }
-    return ans;
-}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     int n;
     cin>>n;
-    vector<int> values(n);
-    for(int i = 0; i < n; ++i) {
-        cin>>values[i];
+    vector<int> v;
+    int ans = 0, curr;
+    for(int i = 0; i < n; i++) {
+        cin>>curr;
+        while(v.size()) {
+            ans++;
+            if(curr > v.back()) v.pop_back();
+            else break;
+        }
+        v.push_back(curr);
     }
-    int answer = totalPairs(n, values);
-    cout<<answer<<endl;
+    cout<<ans<<endl;
     return 0;
 }
