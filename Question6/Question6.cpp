716a99b3780a69a86e8491762e3c6d9779002b72
jiayushe <shejiayu1998@gmail.com>
2021-10-05 05:13:24 +0800

    Improve q6

--- Question6/Question6.cpp
+++ Question6/Question6.cpp
@@ -1,14 +1,39 @@
-#include <iostream>
+#pragma G++ optimize("O3")
+#pragma GCC optimize("O3")
 #include <vector>
 #include <algorithm>
 #include <unordered_map>
 #include <cstring>
+#include <string>
+#include <cstdio>
 
 using namespace std;
 
 #define MAXN 10000
 
 int d[MAXN], cnt[MAXN], fcnt[MAXN], scnt[MAXN], tcnt[MAXN];
+
+inline char getch() {
+    static char buf[100000], *p1 = buf, *p2 = buf;
+    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 100000, stdin), p1 == p2) ? EOF : *p1++;
+}
+
+inline int read() {
+    char c;
+    while((c = getch()) < '0' || c > '9');
+    int res = c - '0';
+    while((c = getch()) >= '0' && c <= '9') res = res * 10 + c - '0';
+    return res;
+}
+
+inline string read_str() {
+    char c;
+    while((c = getch()) == ' ' || c == '\n' || c == '\r' || c == EOF);
+    string res = "";
+    res += c;
+    while((c = getch()) != ' ' && c != '\n' && c != '\r' && c != EOF) res += c;
+    return res;
+}
 
 inline int find(int a) {
     if(d[a] == -1) return a;
@@ -22,25 +47,27 @@ inline void join(int a, int b) {
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n, m, a, b, f, s, t, g, xg, yg;
-    cin>>n>>m>>a>>b>>f>>s>>t;
+    int n = read(), m = read(), a = read(), b = read(), f = read(), s = read(), t = read(), g, xg, yg;
     string x, y;
     unordered_map<int, string> idx_nme;
     unordered_map<string, int> nme_idx;
     memset(d, -1, sizeof d);
-    for(int i = 0; i < n; i++) {
-        cin>>x>>g;
+    for(register int i = 0; i < n; ++i) {
+        x = read_str(), g = read();
         idx_nme[i] = x;
         nme_idx[x] = i;
         cnt[i] = 1;
-        if(g == 1) fcnt[i]++;
-        else if(g == 2) scnt[i]++;
-        else tcnt[i]++;
+        switch(g) {
+            case 1:
+                fcnt[i]++; break;
+            case 2:
+                scnt[i]++; break;
+            case 3:
+                tcnt[i]++;
+        }
     }
-    for(int i = 0; i < m; i++) {
-        cin>>x>>y;
+    for(register int i = 0; i < m; ++i) {
+        x = read_str(), y = read_str();
         xg = find(nme_idx[x]), yg = find(nme_idx[y]);
         if(xg != yg && cnt[xg] + cnt[yg] <= b && fcnt[xg] + fcnt[yg] <= f
             && scnt[xg] + scnt[yg] <= s && tcnt[xg] + tcnt[yg] <= t) {
@@ -53,20 +80,20 @@ int main() {
     }
     int mx = a - 1;
     vector<int> idxs;
-    for(int i = 0; i < n; i++) {
+    for(register int i = 0; i < n; ++i) {
         if(cnt[i] > mx) mx = cnt[i], idxs = {i};
         else if(cnt[i] == mx && idxs.size()) idxs.push_back(i);
     }
     if(idxs.size() == 0) {
-        cout<<"no groups"<<endl;
+        printf("no groups");
         return 0;
     }
     int idx = idxs[0];
     for(auto i : idxs) join(i, idx);
     vector<string> res;
-    for(int i = 0; i < n; i++)
+    for(register int i = 0; i < n; ++i)
         if(find(i) == idx) res.push_back(idx_nme[i]);
     sort(res.begin(), res.end());
-    for(auto i : res) cout<<i<<"\n";
+    for(auto i : res) printf("%s\n", i.c_str());
     return 0;
 }
