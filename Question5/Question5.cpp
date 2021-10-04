69e43e44f66b67fabbdda53beaaa016e3c8e07fa
jiayushe <shejiayu1998@gmail.com>
2021-10-04 10:40:56 +0800

    Clean up q5

--- Question5/Question5.cpp
+++ Question5/Question5.cpp
@@ -3,30 +3,30 @@ using namespace std;
 #include <iostream>
 
 string solution(string n){
-    // code here
     int m = n.length();
     int one = 0, two = 0;
     for(int i = 0; i < m; i++) {
-		int j = i;
-		while(j < m && n[j] == '0') {
-			j++;
-		}
-		int curr = j - i;
-		if(curr > one) {
-			two = one;
-			one = curr;
-		} else if(curr > two) {
-			two = curr;
-		}
-		i = j;
-	}
-	return (one % 2 && one / 2 + 1 > two) ? "A" : "B";
+        int j = i;
+        while(j < m && n[j] == '0') {
+            j++;
+        }
+        int curr = j - i;
+        if(curr > one) {
+            two = one;
+            one = curr;
+        } else if(curr > two) {
+            two = curr;
+        }
+        i = j;
+    }
+    return (one % 2 && one / 2 + 1 > two) ? "A" : "B";
 }
 
-/*  Do not edit below code */
 int main() {
-	string  n;
-	cin >> n;	
-    string answer=solution(n);
-	cout << answer << endl;	
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string n;
+    cin>>n;
+    string answer = solution(n);
+    cout<<answer<<endl;
 }
