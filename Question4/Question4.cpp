504e271e08875ec7dd3b117118c63876bca1dce9
jiayushe <shejiayu1998@gmail.com>
2021-10-05 02:48:55 +0800

    Improve q4

--- Question4/Question4.cpp
+++ Question4/Question4.cpp
@@ -1,24 +1,28 @@
-#include <vector>
-#include <iostream>
+#pragma G++ optimize("O3")
+#include <cstdio>
 
 using namespace std;
 
+inline int read() {
+    char c;
+    while((c = getchar()) < '0' || c > '9');
+    int res = c - '0';
+    while((c = getchar()) >= '0' && c <= '9') res = res * 10 + c - '0';
+    return res;
+}
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n;
-    cin>>n;
-    vector<int> v;
-    int ans = 0, curr;
+    int n = read();
+    int v[n], ans = 0, curr, k = 0;
     for(int i = 0; i < n; i++) {
-        cin>>curr;
-        while(v.size()) {
+        curr = read();
+        while(k) {
             ans++;
-            if(curr > v.back()) v.pop_back();
+            if(curr > v[k - 1]) k--;
             else break;
         }
-        v.push_back(curr);
+        v[k++] = curr;
     }
-    cout<<ans<<endl;
+    printf("%d", ans);
     return 0;
 }
