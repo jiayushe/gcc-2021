17add230998e7bc007f2f7c1a10d56c9dac40834
jiayushe <shejiayu1998@gmail.com>
2021-10-04 11:20:45 +0800

    Clean up q1

--- Question1/Question1.cpp
+++ Question1/Question1.cpp
@@ -1,20 +1,11 @@
 using namespace std;
-#include <string>
 #include <iostream>
 
-int solution(int n){
-    // code here
-    int ans = n / 3 * 2;
-    if(n % 3) {
-		ans++;
-	}
-    return ans;
-}
-
-/*  Do not edit below code */
 int main() {
-	int n;
-	cin >> n;	
-    int answer=solution(n);
-	cout << answer << endl;	
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;	
+    int answer = n / 3 * 2 + (n % 3 > 0);
+    cout<<answer<<endl;	
 }
