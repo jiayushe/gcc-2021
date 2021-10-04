bba25633c84a044410b8417638f0e587ce1c51e8
jiayushe <shejiayu1998@gmail.com>
2021-10-04 23:00:34 +0800

    Clean up q6

--- Question6/Question6.cpp
+++ Question6/Question6.cpp
@@ -2,16 +2,21 @@
 #include <vector>
 #include <algorithm>
 #include <unordered_map>
+#include <cstring>
 
 using namespace std;
 
-inline int find(vector<int>& d, int a) {
+#define MAXN 10000
+
+int d[MAXN], cnt[MAXN], fcnt[MAXN], scnt[MAXN], tcnt[MAXN];
+
+inline int find(int a) {
     if(d[a] == -1) return a;
-    return d[a] = find(d, d[a]);
+    return d[a] = find(d[a]);
 }
 
-inline void join(vector<int>& d, int a, int b) {
-    a = find(d, a), b = find(d, b);
+inline void join(int a, int b) {
+    a = find(a), b = find(b);
     if(a == b) return;
     d[a] = b;
 }
@@ -19,26 +24,27 @@ inline void join(vector<int>& d, int a, int b) {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    int n, m, a, b, f, s, t, g;
+    int n, m, a, b, f, s, t, g, xg, yg;
     cin>>n>>m>>a>>b>>f>>s>>t;
     string x, y;
     unordered_map<int, string> idx_nme;
     unordered_map<string, int> nme_idx;
-    vector<int> d(n, -1), cnt(n, 1), fcnt(n, 0), scnt(n, 0), tcnt(n, 0);
+    memset(d, -1, sizeof d);
     for(int i = 0; i < n; i++) {
         cin>>x>>g;
         idx_nme[i] = x;
         nme_idx[x] = i;
+        cnt[i] = 1;
         if(g == 1) fcnt[i]++;
         else if(g == 2) scnt[i]++;
         else tcnt[i]++;
     }
     for(int i = 0; i < m; i++) {
         cin>>x>>y;
-        int xg = find(d, nme_idx[x]), yg = find(d, nme_idx[y]);
+        xg = find(nme_idx[x]), yg = find(nme_idx[y]);
         if(xg != yg && cnt[xg] + cnt[yg] <= b && fcnt[xg] + fcnt[yg] <= f
             && scnt[xg] + scnt[yg] <= s && tcnt[xg] + tcnt[yg] <= t) {
-            join(d, xg, yg);
+            join(xg, yg);
             cnt[yg] += cnt[xg];
             fcnt[yg] += fcnt[xg];
             scnt[yg] += scnt[xg];
@@ -56,13 +62,11 @@ int main() {
         return 0;
     }
     int idx = idxs[0];
-    for(auto i : idxs) join(d, i, idx);
+    for(auto i : idxs) join(i, idx);
     vector<string> res;
     for(int i = 0; i < n; i++)
-        if(find(d, i) == idx)
-            res.push_back(idx_nme[i]);
+        if(find(i) == idx) res.push_back(idx_nme[i]);
     sort(res.begin(), res.end());
     for(auto i : res) cout<<i<<"\n";
-    cout<<endl;
     return 0;
 }
