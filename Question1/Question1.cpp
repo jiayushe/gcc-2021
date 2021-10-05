1d4a1b3514896a51b0de7696e4655ac2e7cd58c1
jiayushe <shejiayu1998@gmail.com>
2021-10-05 20:14:31 +0800

    Improve q1

--- Question1/Question1.cpp
+++ Question1/Question1.cpp
@@ -1,11 +1,24 @@
+#pragma G++ optimize("O3")
+#pragma GCC optimize("O3")
+#include <cstdio>
+#include <cctype>
+
 using namespace std;
-#include <iostream>
+
+inline char getch() {
+    static char buf[10], *p1 = buf, *p2 = buf;
+    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 10, stdin), p1 == p2) ? EOF : *p1++;
+}
+
+inline int read() {
+    char c;
+    while(!isdigit(c = getch()));
+    int res = c - '0';
+    while(isdigit(c = getch())) res = res * 10 + c - '0';
+    return res;
+}
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n;
-    cin>>n;	
-    int answer = n / 3 * 2 + (n % 3 > 0);
-    cout<<answer<<endl;	
+    int n = read();
+    printf("%d\n", n / 3 * 2 + (n % 3 > 0));
 }
