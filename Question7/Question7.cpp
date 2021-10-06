b8158d1427c12616abc17dbb180c188921b6876a
jiayushe <shejiayu1998@gmail.com>
2021-10-06 09:20:38 +0800

    Improve q7

--- Question7/Question7.cpp
+++ Question7/Question7.cpp
@@ -27,7 +27,7 @@ inline int read() {
 
 const ll mod = 1e9 + 7;
 int prime[] = {0,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997};
-int d[MAXN], prime_grp[MAXV], cnt;
+int d[MAXN], rk[MAXN], prime_grp[MAXV], cnt;
 
 inline ll twopowmod(int p) {
     ll res = 1, a = 2;
@@ -46,7 +46,8 @@ inline int find(int a) {
 inline void join(int a, int b) {
     a = find(a), b = find(b);
     if(a == b) return;
-    d[a] = b;
+    if(rk[a] == rk[b]) --rk[b];
+    (rk[a] > rk[b]) ? d[a] = b : d[b] = a;
 }
 
 inline void update(int i, int f) {
@@ -63,14 +64,13 @@ int main() {
     cnt = n;
     memset(prime_grp, -1, sizeof prime_grp);
     memset(d, -1, sizeof d);
+    memset(rk, -1, sizeof rk);
     for(register int i = 0; i < n; ++i) {
-        curr = read();
-        j = 1, f = prime[j];
-        while(f * f <= curr) {
+        curr = read(), j = 1;
+        while((f = prime[j]) * f <= curr) {
             if(curr % f == 0) update(i, f);
             while(curr % f == 0) curr /= f;
             if(++j == m) break;
-            f = prime[j];
         }
         if(curr != 1) update(i, curr);
     }
