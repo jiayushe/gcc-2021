ea50c6895ff7d90a92a109accd86013e68b28b1a
jiayushe <shejiayu1998@gmail.com>
2021-10-08 03:54:13 +0800

    Improve q9

--- Question9/Question9.cpp
+++ Question9/Question9.cpp
@@ -10,11 +10,14 @@ using namespace std;
 
 #define LSOne(S) ((S) & -(S))
 #define MAXB 10
-#define MAXC 20
+#define MAXC 21
+#define inf 100000
+
+int rsum[MAXB][MAXC][MAXC], memo[1<<MAXB][MAXC];
 
 inline char getch() {
-    static char buf[1000], *p1 = buf, *p2 = buf;
-    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 1000, stdin), p1 == p2) ? EOF : *p1++;
+    static char buf[2000], *p1 = buf, *p2 = buf;
+    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 2000, stdin), p1 == p2) ? EOF : *p1++;
 }
 
 inline int read() {
@@ -25,36 +28,10 @@ inline int read() {
     return res;
 }
 
-const int inf = 1e9;
-int b, c;
-int rsum[MAXB][MAXC][MAXC];
-int memo[MAXC][1<<MAXB];
-
-int solve(int curr, int mask) {
-    if(curr == c) return 0;
-    if(mask == 0) return inf;
-    int& ans = memo[curr][mask];
-    if(ans != -1) return ans;
-    ans = inf;
-    int m = mask;
-    while(m) {
-        int pow_v = LSOne(m);
-        int new_mask = mask ^ pow_v;
-        int v = __builtin_ctz(pow_v);
-        auto& curr_rsum = rsum[v];
-        ans = min(ans, solve(curr, new_mask));
-        for(register int nxt = curr; nxt < c; ++nxt) {
-            ans = min(ans, curr_rsum[curr][nxt] + solve(nxt + 1, new_mask));
-        }
-        m ^= pow_v;
-    }
-    return ans;
-}
-
 int main() {
-    c = read(), b = read();
+    int c = read(), b = read();
     memset(rsum, 0, sizeof rsum);
-    memset(memo, -1, sizeof memo);
+    memset(memo, 0, sizeof memo);
     for(register int i = 0; i < b; ++i) {
         auto& curr_rsum = rsum[i];
         curr_rsum[0][0] = read();
@@ -67,6 +44,25 @@ int main() {
             }
         }
     }
-    printf("%d", solve(0, (1 << b) - 1));
+    for(register int curr = 0; curr < c; ++curr) memo[0][curr] = inf;
+    for(register int mask = 1; mask < (1<<b); ++mask) {
+        auto& curr_memo = memo[mask];
+        for(register int curr = c - 1; curr >= 0; --curr) {
+            int& ans = curr_memo[curr] = inf;
+            int m = mask;
+            while(m) {
+                int pow_v = LSOne(m);
+                int new_mask = mask ^ pow_v;
+                int v = __builtin_ctz(pow_v);
+                auto& curr_rsum = rsum[v];
+                ans = min(ans, memo[new_mask][curr]);
+                for(register int nxt = curr; nxt < c; ++nxt) {
+                    ans = min(ans, curr_rsum[curr][nxt] + memo[new_mask][nxt + 1]);
+                }
+                m ^= pow_v;
+            }
+        }
+    }
+    printf("%d", memo[(1<<b) - 1][0]);
     return 0;
 }
