67325f38de0c51075eb4e51afdebc61c0cb0e188
jiayushe <shejiayu1998@gmail.com>
2021-10-08 23:19:40 +0800

    Improve q3

--- Question3/Question3.cpp
+++ Question3/Question3.cpp
@@ -1,35 +1,52 @@
+#pragma G++ optimize("O3")
+#pragma GCC optimize("O3")
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
+inline char getch() {
+    static char buf[100000], *p1 = buf, *p2 = buf;
+    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 100000, stdin), p1 == p2) ? EOF : *p1++;
+}
+
+inline int read() {
+    char c;
+    while(!isdigit(c = getch()) && c != EOF);
+    if(c == EOF) return 0;
+    int res = c ^ 48;
+    while(isdigit(c = getch())) res = res * 10 + (c ^ 48);
+    return res;
+}
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n, m, x;
-    cin>>n>>m>>x;
-    vector<int> a(n), b(m);
-    for(int i = 0; i < n; i++) cin>>a[i];
-    for(int i = 0; i < m; i++) cin>>b[i];
-    if(n > m) swap(a, b), swap(n, m);
-    vector<int> ap, bp;
-    ap.push_back(x);
-    for(int i = 0; i < n; i++) {
-        if(a[i] > ap[i]) break;
-        ap.push_back(ap[i] - a[i]);
+    int n = read(), m = read(), x = read(), a[++n], b[++m], n2 = n, m2 = m;
+    a[0] = b[0] = 0;
+    for(register int i = 1; i < n; ++i) {
+        a[i] = a[i - 1] + read();
+        if(a[i] >= x) {
+            n2 = i + (a[i] == x);
+            while(++i < n) read();
+        }
     }
-    bp.push_back(0);
-    for(int i = 0; i < m; i++) {
-        if(b[i] > x - bp[i]) break;
-        bp.push_back(bp[i] + b[i]);
+    for(register int i = 1; i < m; ++i) {
+        b[i] = b[i - 1] + read();
+        if(b[i] >= x) {
+            m2 = i + (b[i] == x);
+            while(++i < m) read();
+        }
     }
-    n = ap.size();
-    int ans = 0;
-    for(int i = 0; i < n; i++) {
-        int j = upper_bound(bp.begin(), bp.end(), ap[i]) - bp.begin();
-        ans = max(ans, i + j - 1);
-    }
-    cout<<ans<<endl;
+    int ans = 0, j;
+    if(n2 <= m2)
+        for(register int i = 0; i < n2; ++i) {
+            j = upper_bound(b, b + m2, x - a[i]) - b;
+            ans = max(ans, i + j - 1);
+        }
+    else
+        for(register int i = 0; i < m2; ++i) {
+            j = upper_bound(a, a + n2, x - b[i]) - a;
+            ans = max(ans, i + j - 1);
+        }
+    printf("%d", ans);
     return 0;
 }
