c53c92ddc92878ea59a33d5cc4b4fd36dc140d77
jiayushe <shejiayu1998@gmail.com>
2021-10-08 09:26:10 +0800

    Improve q2

--- Question2/Question2.cpp
+++ Question2/Question2.cpp
@@ -1,29 +1,37 @@
+#pragma G++ optimize("O3")
+#pragma GCC optimize("O3")
 #include <iostream>
-#include <sstream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
+inline char getch() {
+    static char buf[1000], *p1 = buf, *p2 = buf;
+    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 1000, stdin), p1 == p2) ? EOF : *p1++;
+}
+
+inline int read() {
+    char c;
+    while(!isdigit(c = getch()) && c != EOF);
+    if(c == EOF) return 0;
+    int res = c ^ 48;
+    while(isdigit(c = getch())) res = res * 10 + (c ^ 48);
+    return res;
+}
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    int z, data, n = 0, ans = 0;
-    cin>>z;
-    cin.ignore();
-    string buffer;
-    getline(cin, buffer);
-    istringstream iss(buffer);
+    int z = read(), data, n = 0, ans = 0;
     vector<pair<int, int>> v;
-    while(iss>>data) v.push_back({data, ++n});
+    while((data = read())) v.push_back({data, ++n});
     sort(v.begin(), v.end());
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; ++i) {
         auto& curr = v[i];
         if(z < curr.first) break;
         int x = min(z / curr.first, curr.second);
         ans += x;
         z -= x * curr.first;
     }
-    cout<<ans<<endl;
+    printf("%d", ans);
     return 0;
 }
