5f0e693e839168111a1b1ef12d37bbe46550fe79
jiayushe <shejiayu1998@gmail.com>
2021-10-10 21:23:00 +0800

    Improve q5

--- Question5/Question5.cpp
+++ Question5/Question5.cpp
@@ -1,27 +1,36 @@
-#include <string>
+#pragma G++ optimize("O3")
+#pragma GCC optimize("O3")
 #include <iostream>
 
 using namespace std;
 
+#define MAXN 100000
+
+inline char getch() {
+    static char buf[MAXN], *p1 = buf, *p2 = buf;
+    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, MAXN, stdin), p1 == p2) ? EOF : *p1++;
+}
+
+inline string read_str() {
+    char c;
+    while(isspace(c = getch()) || c == EOF);
+    string res = "";
+    res += c;
+    while(!isspace(c = getch()) && c != EOF) res += c;
+    return res;
+}
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    string s;
-    cin>>s;
+    string s = read_str();
     int n = s.length();
     int one = 0, two = 0;
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; ++i) {
         int j = i;
-        while(j < n && s[j] == '0') j++;
+        while(j < n && s[j] == '0') ++j;
         int curr = j - i;
-        if(curr > one) {
-            two = one;
-            one = curr;
-        } else if(curr > two) {
-            two = curr;
-        }
+        if(curr > one) two = one, one = curr;
+        else if(curr > two) two = curr;
         i = j;
     }
-    string ans = (one % 2 && one / 2 + 1 > two) ? "A" : "B";
-    cout<<ans<<endl;
+    putchar(((one & 1) && (one >> 1) + 1 > two) ? 'A' : 'B');
 }
