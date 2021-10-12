be5f58e501037450edb0a3a6eb2863f12490327e
jiayushe <shejiayu1998@gmail.com>
2021-10-13 07:58:40 +0800

    Add q4 in c

--- Question4/Question4.c
+++ Question4/Question4.c
@@ -1,19 +1,38 @@
+#pragma GCC optimize("O3")
 #include <stdio.h>
-int totalPairs(int n, int values[]){
-    // Complete the total Pairs function
-    return -1;
+#include <ctype.h>
+
+#define SZ 100000
+
+char buf[SZ], *p1 = buf, *p2 = buf;
+
+char getch();
+int read();
+
+inline char getch() {
+    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, SZ, stdin), p1 == p2) ? EOF : *p1++;
 }
-int main(){
-    int n, i;
-    scanf("%d", &n);
-    int answer;
-    int values[n];
-    for (i = 0; i < n; ++i) {
-        scanf("%d", &values[i]);
+
+inline int read() {
+    char c;
+    while(!isdigit(c = getch()));
+    int res = c ^ 48;
+    while(isdigit(c = getch())) res = res * 10 + (c ^ 48);
+    return res;
+}
+
+int main() {
+    int n = read();
+    int v[n], ans = 0, curr, k = 0;
+    for(int i = 0; i < n; ++i) {
+        curr = read();
+        while(k) {
+            ++ans;
+            if(curr > v[k - 1]) --k;
+            else break;
+        }
+        v[k++] = curr;
     }
-	answer = totalPairs(n, values);
-    // Do not remove below line
-    printf("%d",answer);
-    // Do not print anything after this line
+    printf("%d", ans);
     return 0;
 }
